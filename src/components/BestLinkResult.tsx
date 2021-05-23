9f31359054305d8dd02d3ca10a03d1647f6b8b01
Tähkävuori Vesa <vesa.tahkavuori@cinia.fi>
2021-05-23 11:23:18 +0300

    remove trailing zeroes

--- src/components/BestLinkResult.tsx
+++ src/components/BestLinkResult.tsx
@@ -10,7 +10,7 @@ interface Props {
  * This component is only shown when a device with given coordinates has a station within reach
 */
 const BestLinkResult: React.FC<Props> = ({ result }) => {
-  return <div> {`Best link station for point ${result.pointX}, ${result.pointY} is ${result.stationX}, ${result.stationY} with power ${result.powerToLinkStation.toFixed(2)}`}</div>;
+  return <div> {`Best link station for point ${result.pointX}, ${result.pointY} is ${result.stationX}, ${result.stationY} with power ${parseFloat(result.powerToLinkStation.toFixed(5))}`}</div>;
 };
 
 export default BestLinkResult;
